3f58b3ccd469c57e2b145ac43b7d8971ccbf502e
iactest <103730049+iactest@users.noreply.github.com>
2022-06-17 14:15:57 +0530

    Update s3_logging.tf

--- s3_logging.tf
+++ s3_logging.tf
@@ -6,3 +6,7 @@ resource "aws_s3_bucket" "examplebucket" {
 resource "aws_s3_bucket" "examplebucket100" {
   bucket = "examplebuckettftest100"
 }
+
+resource "aws_s3_bucket" "examplebucket200" {
+  bucket = "examplebuckettftest200"
+}
